0fd0f77e6ac60020cea5fb2896d4e2b6b1299b39
Clinton Bradford <clintonbradford@gmail.com>
2020-07-04 18:40:09 -0400

    Add star spinner demo

---
+++ starspinner/precache-manifest.110c6a569c15738370dac04d4dd859d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57f4d6c47ea36add5cf926e529c299d",
+    "url": "/starspinner/index.html"
+  },
+  {
+    "revision": "4f0d7a105b32c9ad1557",
+    "url": "/starspinner/static/css/main.aea7a401.chunk.css"
+  },
+  {
+    "revision": "2e443f092ffd608ff145",
+    "url": "/starspinner/static/js/2.e7c23ca7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starspinner/static/js/2.e7c23ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0d7a105b32c9ad1557",
+    "url": "/starspinner/static/js/main.40f8969d.chunk.js"
+  },
+  {
+    "revision": "448598ff690756f2aa3e",
+    "url": "/starspinner/static/js/runtime-main.a3c38e50.js"
+  }
+]);
